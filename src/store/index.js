82c977ce366a2e39e226b7b06e4758ee4f7ea631
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-09-01 21:15:55 +0300

    Create auth.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,5 @@
 import { createSlice, configureStore } from '@reduxjs/toolkit';
 
-const initialAuthState = {
-    isAuthenticated: false,
-};
-
-const authSlice = createSlice({
-    name: 'auth',
-    initialState: initialAuthState,
-    reducers: {
-        login(state) {
-            state.isAuthenticated = true;
-        },
-        logout(state) {
-            state.isAuthenticated = false;
-        }
-    }
-});
-
 /*const counterReducer = (state = initialState, action) => {
     if (action.type === 'increment') {
         return {
